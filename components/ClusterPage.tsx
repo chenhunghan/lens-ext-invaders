86f18638aed29e93961c60969473253408f2484b
Hung-Han (Henry) Chen <1474479+chenhunghan@users.noreply.github.com>
2020-12-07 19:40:00 +0800

    Add useLayoutEffect
    Signed-off-by: Hung-Han (Henry) Chen <1474479+chenhunghan@users.noreply.github.com>

--- components/ClusterPage.tsx
+++ components/ClusterPage.tsx
@@ -1,27 +1,35 @@
-import React from "react"
-import p5 from 'p5';
+import React, { useState, useLayoutEffect } from "react"
+import p5 from "p5";
 
 const sketch = (p: p5) => {
-    let x = 300;
-    let y = 300;
-    p.setup = () => {
-        p.createCanvas(p.windowWidth, p.windowHeight);
-    };
+  const x = 300;
+  const y = 300;
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.frameRate(30);
+  };
 
-    p.draw = () => {
-        p.background(0);
-        p.fill(255);
-        p.rect(x, y, 50, 50);
-    };
+  p.draw = () => {
+    p.background(0);
+    p.fill(255);
+    p.rect(x, y, 50, 50);
+  };
 };
 
-new p5(sketch);
 
 const ClusterPage = (): JSX.Element => {
-    return (
-        <>
-        </>
-    )
+  const [init, setInit] = useState(false);
+  useLayoutEffect(() => {
+    if (!init) {
+      new p5(sketch, document.getElementById("p5_canvas_container"));
+      console.info("👾👾👾👾👾👾👾")
+      setInit(true)
+    }
+  }, [init]);
+  return (
+    <div id='p5_canvas_container'>
+    </div>
+  )
 }
 
 export default ClusterPage

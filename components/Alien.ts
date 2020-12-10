23863223b439e370c91afd1efb43cfe78ec1df58
Hung-Han (Henry) Chen <1474479+chenhunghan@users.noreply.github.com>
2020-12-10 13:39:23 +0800

    Connect pod state with alien
    Signed-off-by: Hung-Han (Henry) Chen <1474479+chenhunghan@users.noreply.github.com>

--- components/Alien.ts
+++ components/Alien.ts
@@ -20,14 +20,55 @@ class Alien {
   }
 
   draw(): void {
-    this.p5.image(this.alienImages.greenAlien, this.x, this.y, this.alienImages.greenAlien.width / 20, this.alienImages.greenAlien.height / 20);
+    let image: p5.Image;
     this.p5.textSize(8);
-    [...this.pod.metadata.name].forEach((character, i, n) => {
-      if (i % 7 === 0) {
-        this.p5.text(`${n[i - 3] ?? ""}${n[i - 2] ?? ""}${n[i - 1] ?? ""}${character}${n[i + 1] ?? ""}${n[i + 2] ?? ""}${n[i + 3] ?? ""}`, this.x, this.y + 40 + (i / 7) * 10);
-      }
-    })
 
+    if (
+      this.pod.status.containerStatuses &&
+      this.pod.status.containerStatuses[0] && 
+      this.pod.status.containerStatuses[0].state
+    ) {
+      const states = this.pod.status.containerStatuses[0].state;
+      
+      if ("running" in states) {
+        image = this.alienImages.greenAlien
+      }
+      if ("waiting" in states) {
+        console.log("states", states)
+        image = this.alienImages.yellowAlien
+      }
+      if ("terminated" in states) {
+        image = this.alienImages.redAlien
+      }
+      for (const state in states) {
+        this.p5.text(state, this.x, this.y + 50);
+      }
+      const conditions = this.pod.status.conditions
+      this.p5.textSize(6);
+      this.p5.text(conditions[conditions.length - 1].type, this.x - 5, this.y + 60)
+      this.p5.textSize(8);
+    } else if (this.pod.status.phase && this.pod.status.phase === "Pending") {
+      // there could condition that this.pod.status.containerStatuses is undefined and
+      // this.pod.status.phase === "Pending"
+      image = this.alienImages.orangeAlien
+      this.p5.textSize(6);
+      this.p5.text(this.pod.status.phase, this.x - 5, this.y + 70)
+      this.p5.textSize(8);
+    }
+
+    image && this.p5.image(image, this.x, this.y, image.width / 20, image.height / 20);
+
+    if (
+      this.pod.spec.containers &&
+      this.pod.spec.containers[0]
+    ) {
+      const name = this.pod.spec.containers[0].name
+      if (name && name.length > 7) {
+        this.p5.text(`${name.substring(0, 7)}..`, this.x, this.y + 40);
+      } else {
+        this.p5.text(`${name}`, this.x, this.y + 40);
+      }
+    }
   }
 }
 
